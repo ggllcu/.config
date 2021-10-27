600a52b512bb773f1d32632eb7d7fb6b8d38d8b8
Luca Guglielmi <luca.guglielmi@intesys.it [push]>
2021-10-28 01:29:28 +0200

    Line

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,35 +3,37 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin', '~/.config/nvim/plugged')
 
-Plug( 'nvim-lua/popup.nvim') -- required by telescope
-Plug( 'nvim-lua/plenary.nvim') -- required by telescope
-Plug( 'nvim-telescope/telescope.nvim')
-Plug( 'nvim-telescope/telescope-fzy-native.nvim')
+Plug('nvim-lua/popup.nvim') -- required by telescope
+Plug('nvim-lua/plenary.nvim') -- required by telescope
+Plug('nvim-telescope/telescope.nvim')
+Plug('nvim-telescope/telescope-fzy-native.nvim')
 
-Plug( 'nvim-treesitter/nvim-treesitter')
+Plug('nvim-treesitter/nvim-treesitter')
 
-Plug( 'neovim/nvim-lspconfig')
+Plug('neovim/nvim-lspconfig')
 
-Plug( 'hrsh7th/cmp-nvim-lsp' )
-Plug( 'hrsh7th/cmp-nvim-lua' )
-Plug( 'hrsh7th/cmp-buffer' )
-Plug( 'hrsh7th/cmp-path' )
-Plug( 'tamago324/cmp-zsh' )
-Plug( 'hrsh7th/nvim-cmp' )
+Plug('hrsh7th/cmp-nvim-lsp' )
+Plug('hrsh7th/cmp-nvim-lua' )
+Plug('hrsh7th/cmp-buffer' )
+Plug('hrsh7th/cmp-path' )
+Plug('tamago324/cmp-zsh' )
+Plug('hrsh7th/nvim-cmp' )
 
-Plug( 'onsails/lspkind-nvim' )
+Plug('onsails/lspkind-nvim' )
 
-Plug( 'hoob3rt/lualine.nvim')
+Plug('hoob3rt/lualine.nvim')
 
-Plug( 'kyazdani42/nvim-web-devicons')  -- for file icons
-Plug( 'kyazdani42/nvim-tree.lua')
+Plug('kyazdani42/nvim-web-devicons')  -- for file icons
+Plug('kyazdani42/nvim-tree.lua')
 
-Plug( 'tpope/vim-surround')
-Plug( 'tpope/vim-commentary')
-Plug( 'mattn/emmet-vim')
-Plug( 'mhartington/oceanic-next')
-Plug( 'jiangmiao/auto-pairs')
-Plug( 'ray-x/lsp_signature.nvim')
+Plug('tpope/vim-surround')
+Plug('tpope/vim-commentary')
+Plug('mattn/emmet-vim')
+Plug('mhartington/oceanic-next')
+Plug('jiangmiao/auto-pairs')
+Plug('ray-x/lsp_signature.nvim')
+
+Plug('akinsho/bufferline.nvim')
 
 vim.call('plug#end')
 
